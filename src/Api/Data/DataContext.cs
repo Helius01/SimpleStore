3d21d5856f1c2f6e1a9dbbefccf9397e1b4bf257
kalepook <kooroosh.kabir1@gmail.com>
2023-12-11 14:03:41 +0330

    fix : data context configured

---
+++ src/Api/Data/DataContext.cs
@@ -0,0 +1,21 @@
+using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using SimpleShop.src.Api.Domains;
+
+namespace SimpleShop.src.Api.Data;
+
+public class DataContext : DbContext
+{
+    public DbSet<User> Users { get; set; } = default!;
+    public DbSet<Product> Products { get; set; } = default!;
+    public DbSet<Order> Orders { get; set; } = default!;
+
+    public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+
+}
--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using SimpleShop.src.Api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var postgresConnectionString = builder.Configuration.GetConnectionString("PostgreSQL");
+
+builder.Services.AddDbContext<DataContext>(options =>
+                options.UseNpgsql(postgresConnectionString ?? throw new ArgumentNullException(postgresConnectionString)));
 
 var app = builder.Build();
 
